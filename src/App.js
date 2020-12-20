02839a1c3f08e48b086d505e155b50bea74fce62
pashapushak1996 <pashapushak1996@gmail.com>
2020-12-20 15:30:24 +0200

    master

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-export default function App() {
-    return (
-        <div>
+import React, {Component} from "react";
+import './App.css'
 
+const App = () => {
+    return (
+        <div className={`app-wrapper`}>
+            <header className={`header`}>
+                <img src="https://cdn.worldvectorlogo.com/logos/slack.svg" alt="2"/>
+            </header>
+            <nav className={`nav`}>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>News</div>
+                <div>Music</div>
+                <div>Settings</div>
+            </nav>
+            <div className={`content`}>
+                <div>
+                    <img
+                        src="https://static9.depositphotos.com/1594308/1110/i/600/depositphotos_11107478-stock-photo-fantasy.jpg"
+                        alt=""/>
+                </div>
+                <div>
+                    ava+description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>Post 1</div>
+                        <div>Post 2</div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 }
 
+export default App;
